4b703668b6fff0a265cf9268dc1826d1d632b51b
Yernar24 <yernar.zhumygaliyev@gmail.com>
2024-05-31 18:51:10 +0500

    Complete assignment 1

---
+++ README.txt
@@ -0,0 +1,3 @@
+# Module 1 Solution
+
+This is the solution for the first assignment of the Single Page Application with AngularJS course. The application checks if the number of items entered for lunch is too much or not.
---
+++ app.js
@@ -0,0 +1,24 @@
+(function() {
+    angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+  
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+      $scope.checkIfTooMuch = function() {
+        if (!$scope.lunchItems) {
+          $scope.message = "Please enter the data first";
+          return;
+        }
+  
+        var items = $scope.lunchItems.split(',').filter(item => item.trim() !== '');
+        if (items.length === 0) {
+          $scope.message = "Please enter the data first";
+        } else if (items.length <= 3) {
+          $scope.message = "Enjoy your meal!";
+        } else {
+          $scope.message = "Too much!";
+        }
+      };
+    }
+  })();
+  
